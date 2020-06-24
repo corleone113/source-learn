c16feb992aa652b56a76414d2916a9abe95aa1f6
corleone113 <corleonexiao@qq.com>
2020-06-24 22:41:21 +0800

    补充react-router-config的阅读注释

--- react-related/src/source/react-router-config/matchRoutes.js
+++ react-related/src/source/react-router-config/matchRoutes.js
@@ -1,17 +1,17 @@
 import { matchPath, Router } from "react-router";
 
-function matchRoutes(routes, pathname, /*not public API*/ branch = []) {
-  routes.some(route => {
+function matchRoutes(routes, pathname, /*not public API*/ branch = []) { // 收集route配置数组中和指定pathname匹配的记录汇总为一个数组
+  routes.some(route => { // 找到第一个匹配的路由就结束遍历
     const match = route.path
-      ? matchPath(pathname, route)
+      ? matchPath(pathname, route) // 优先使用pathname和route.path的匹配结果
       : branch.length
       ? branch[branch.length - 1].match // use parent match
       : Router.computeRootMatch(pathname); // use default "root" match
 
     if (match) {
-      branch.push({ route, match });
+      branch.push({ route, match }); // 匹配成功则添加一条记录
 
-      if (route.routes) {
+      if (route.routes) { // 存在子路由则递归调用matchRoutes进行处理
         matchRoutes(route.routes, pathname, branch);
       }
     }
--- react-related/src/source/react-router-config/renderRoutes.js
+++ react-related/src/source/react-router-config/renderRoutes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router";
 
-function renderRoutes(routes, extraProps = {}, switchProps = {}) {
+function renderRoutes(routes, extraProps = {}, switchProps = {}) { // 将路由配置数组渲染为<Switch>包裹的多个<Route>
   return routes ? (
     <Switch {...switchProps}>
       {routes.map((route, i) => (
@@ -10,11 +10,11 @@ function renderRoutes(routes, extraProps = {}, switchProps = {}) {
           path={route.path}
           exact={route.exact}
           strict={route.strict}
-          render={props =>
+          render={props => // 通过render prop渲染
             route.render ? (
-              route.render({ ...props, ...extraProps, route: route })
+              route.render({ ...props, ...extraProps, route: route }) // 路由配置以route prop传入路由组件
             ) : (
-              <route.component {...props} {...extraProps} route={route} />
+              <route.component {...props} {...extraProps} route={route} /> // 路由配置以route prop传入路由组件
             )
           }
         />
